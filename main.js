8eef66fcafe26c9d1134f452d42d442ff7bed2b0
Agata Szulc <agataszulc97@gmail.com>
2022-03-28 12:55:44 +0200

    basic text + working fetch

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import "./sass/style.scss";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const url = "http://agata.dk/portfolio-db/wp-json/wp/v2/project?_embed";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => data.forEach(showProjects));
+
+function showProjects(project) {
+  document.querySelector(".project_card > h4").textContent = project.title.rendered;
+  document.querySelector(".project_card > img").src =
+    project._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+}
+
+console.log("ho");
