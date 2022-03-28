ca1982133ada5998c46707817f53491308b3d5bd
Agata Szulc <agataszulc97@gmail.com>
2022-03-28 13:43:27 +0200

    projects displayed on main page

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ import "./sass/style.scss";
 const url = "http://agata.dk/portfolio-db/wp-json/wp/v2/project?_embed";
 
 fetch(url)
-  .then((response) => response.json())
+  .then((res) => res.json())
   .then((data) => data.forEach(showProjects));
 
 function showProjects(project) {
-  document.querySelector(".project_card > h4").textContent = project.title.rendered;
-  document.querySelector(".project_card > img").src =
-    project._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector("h4").textContent = project.projtitle;
+  clone.querySelector("img").src = project._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+  document.querySelector(".cards").appendChild(clone);
 }
-
-console.log("ho");
